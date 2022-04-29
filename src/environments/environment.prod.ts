4126cbadf175bb75e4811fe028dc320f41db7faf
LuccaMariani <juanlucamariani@gmail.com>
2022-04-28 23:24:13 -0300

    push a heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBdrTcGOZyYwS9ygSCfcKTi0-L2ZZ6Xe5A",
+    authDomain: "tp-salajuegos-utn.firebaseapp.com",
+    projectId: "tp-salajuegos-utn",
+    storageBucket: "tp-salajuegos-utn.appspot.com",
+    messagingSenderId: "43829926878",
+    appId: "1:43829926878:web:731c649bc90e018eb69b4f"
+  }
 };
